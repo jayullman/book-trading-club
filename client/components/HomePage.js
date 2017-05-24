11e6fddbfde5144f0d632b56cc57cf3a6a807a0a
Jay Ullman <jayullman@gmail.com>
2017-05-23 23:26:14 -0700

    Add home page content and styles

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/homepage.css';
 
 const HomePage = () => (
-  <div>
-    <div>This is the home page</div>
+  <div className='home-page'>
+    <h2>Collect and Trade Books with Other Users!</h2>
     <p>
-      Blurb about app goes here
+      Welcome to the club! This app allows you to trade books with other users. You must 
+      first <Link className='signup-link' to='/signup'>sign up</Link> and <Link className='login-link' to='/login'>log in</Link> before 
+      you can add books to your collection and request trades. You can accept or reject trades
+      requested from other users.
+    </p>
+    <p>
+      This fullstack application was written in node/express on the backend and utilizes a MongoDB
+      for data storage. The front end was created with ReactJS. Check out the source code at this
+      project's <a target='_blank'href='https://github.com/libeja/book-trading-club'>GitHub Page</a>.
     </p>
   </div>
 );
